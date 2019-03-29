0e9a8589655f4f122787e58c3b345b01d7de204a
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-03-29 13:57:59 +0100

    Configuration fix

--- src/main/kotlin/no/nav/helse/Configuration.kt
+++ src/main/kotlin/no/nav/helse/Configuration.kt
@@ -2,89 +2,53 @@ package no.nav.helse
 
 import io.ktor.config.ApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import no.nav.helse.dusseldorf.ktor.core.getOptionalList
+import no.nav.helse.dusseldorf.ktor.core.getRequiredString
 import java.net.URL
-
-private val logger: Logger = LoggerFactory.getLogger("nav.Configuration")
 
 @KtorExperimentalAPI
 data class Configuration(private val config : ApplicationConfig) {
-    private fun getString(key: String,
-                          secret: Boolean = false) : String  {
-        val stringValue = config.property(key).getString()
-        logger.info("{}={}", key, if (secret) "***" else stringValue)
-        return stringValue
-    }
-
-    private fun <T>getListFromCsv(key: String,
-                                  secret: Boolean = false,
-                                  builder: (value: String) -> T) : List<T> {
-        val csv = getString(key, false)
-        val list = csv.replace(" ", "").split(",")
-        val builtList = mutableListOf<T>()
-        list.forEach { entry ->
-            logger.info("$key entry = ${if (secret) "***" else entry}")
-            builtList.add(builder(entry))
-        }
-        return builtList.toList()
-    }
-
     fun getAuthorizedSystemsForRestApi(): List<String> {
-        return getListFromCsv(
+        return config.getOptionalList(
             key = "nav.rest_api.authorized_systems",
-            builder = { value -> value}
+            builder = { value -> value},
+            secret = false
         )
     }
 
     fun getOppgaveBaseUrl() : URL {
-        return URL(getString("nav.oppgave.base_url"))
+        return URL(config.getRequiredString("nav.oppgave.base_url", secret = false))
     }
 
     fun getSparkelBaseUrl() : URL {
-        return URL(getString("nav.sparkel.base_url"))
+        return URL(config.getRequiredString("nav.sparkel.base_url", secret = false))
     }
 
     fun getTokenUrl() : URL {
-        return URL(getString("nav.authorization.token_url"))
+        return URL(config.getRequiredString("nav.authorization.token_url", secret = false))
     }
 
     fun getJwksUrl() : URL {
-        return URL(getString("nav.authorization.jwks_url"))
+        return URL(config.getRequiredString("nav.authorization.jwks_url", secret = false))
     }
 
     fun getIssuer() : String {
-        return getString("nav.authorization.issuer")
+        return config.getRequiredString("nav.authorization.issuer", secret = false)
     }
 
     fun getServiceAccountClientId(): String {
-        return getString("nav.authorization.service_account.client_id")
+        return config.getRequiredString("nav.authorization.service_account.client_id", secret = false)
     }
 
     fun getServiceAccountClientSecret(): String {
-        return getString(key = "nav.authorization.service_account.client_secret", secret = true)
+        return config.getRequiredString(key = "nav.authorization.service_account.client_secret", secret = true)
     }
 
     fun getServiceAccountScopes(): List<String> {
-        return getListFromCsv(
+        return config.getOptionalList(
             key = "nav.authorization.service_account.scopes",
-            builder = { value -> value}
+            builder = { value -> value},
+            secret = false
         )
-    }
-
-    fun logIndirectlyUsedConfiguration() {
-        logger.info("# Indirectly used configuration")
-        val properties = System.getProperties()
-        logger.info("## System Properties")
-        properties.forEach { key, value ->
-            if (key is String && (key.startsWith(prefix = "http", ignoreCase = true) || key.startsWith(prefix = "https", ignoreCase = true))) {
-                logger.info("$key=$value")
-            }
-        }
-        logger.info("## Environment variables")
-        val environmentVariables = System.getenv()
-        logger.info("HTTP_PROXY=${environmentVariables["HTTP_PROXY"]}")
-        logger.info("HTTPS_PROXY=${environmentVariables["HTTPS_PROXY"]}")
-        logger.info("NO_PROXY=${environmentVariables["NO_PROXY"]}")
     }
 }
