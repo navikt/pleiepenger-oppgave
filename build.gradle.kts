7e4b6fb03f0f5dc2cb21ce748c12d3ab68761276
Erik M.Forsman <fraadsbrandth@users.noreply.github.com>
2019-04-03 13:43:23 +0200

    dusseldorf-versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = ext.get("ktorVersion").toString()
-val dusseldorfKtorVersion = "1.1.3.525b270"
+val dusseldorfKtorVersion = "1.1.3.2d0b51a"
 val wiremockVersion = "2.19.0"
 
 val mainClass = "no.nav.helse.PleiepengerOppgaveKt"
@@ -12,7 +12,7 @@ plugins {
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/525b2709f5f4e2f046d802812b7b8b89f5a66a17/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/2d0b51a6956d1ff797e37527232211027f6970ff/gradle/dusseldorf-ktor.gradle.kts")
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
     }
